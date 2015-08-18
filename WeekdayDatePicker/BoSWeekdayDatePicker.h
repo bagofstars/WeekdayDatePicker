325e5ca3f22d2d0b9b3c295445473e74d32c2f7e
bagofstars <glprog@gmail.com>
2015-08-18 21:09:13 +0100

    added main header file

---
+++ WeekdayDatePicker/BoSWeekdayDatePicker.h
@@ -0,0 +1,11 @@
+//
+//  BoSWeekdayDatePicker.h
+//  WeekdayDatePicker
+//
+//  Created by Marcin Hawro on 18/08/2015.
+//  Copyright (c) 2015 Marcin Hawro. All rights reserved.
+//
+
+#import "BoSWeekdayDatePickerView.h"
+#import "BoSWeekdayDatePickerCalendar.h"
+#import "NSDate+BoSConversion.h"
