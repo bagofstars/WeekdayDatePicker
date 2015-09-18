9de057d422ef3375d5d3ce30a19322f96860a869
bagofstars <glprog@gmail.com>
2015-09-18 23:22:21 +0100

    refactored and extended example view controller to demonstrate new callback functionality

--- Example/ExampleClasses/ViewController.m
+++ Example/ExampleClasses/ViewController.m
@@ -12,14 +12,19 @@
 #import "BosWeekdayDatePickerView.h"
 #import "BoSWeekdayDatePickerCalendar.h"
 #import "NSDate+BoSConversion.h"
+#import "BoSWeekdayDatePickerSelectedItems.h"
+
+static NSString *const BoSWeekdayDatePickerExampleDateFormatString = @"EEEE dd MMMM yyyy";
 
 @interface ViewController ()
 
 @property (nonatomic, weak) IBOutlet BoSWeekdayDatePickerView *pickerView;
 @property (nonatomic, weak) IBOutlet UILabel *selectedDateLabel;
 
+@property (nonatomic, strong) NSDateFormatter *dateFormatter;
 
 @end
+
 
 @implementation ViewController
 
@@ -27,20 +32,67 @@
 {
   [super viewDidLoad];
 
-  NSDate *initialSelectionDate = [NSDate bos_dateInCalendar:[BoSWeekdayDatePickerCalendar sharedInstance].calendar
-                                                    fromDay:15 month:12 year:2015];
-  
-  NSDate *minDate = [NSDate bos_dateInCalendar:[BoSWeekdayDatePickerCalendar sharedInstance].calendar
-                                       fromDay:1 month:10 year:2015];
-  
-  NSDate *maxDate = [NSDate bos_dateInCalendar:[BoSWeekdayDatePickerCalendar sharedInstance].calendar
-                                       fromDay:31 month:12 year:2015];
-  
-  BoSWeekdayDatePickerView *sourceCodePickerView = [[BoSWeekdayDatePickerView alloc] initWithFrame:CGRectMake(0.0, 306.0f, CGRectGetWidth([UIScreen mainScreen].bounds), 216.0f)];
-  [sourceCodePickerView setupMinDate:minDate maxDate:maxDate initialSelectionDate:initialSelectionDate];
-  
+  [self setupDateFormatter];
+  [self displayInitialDateOnTheLabel];
+
+  [self setupWeekdayDatePicker];
+}
+
+- (void)setupDateFormatter
+{
+  self.dateFormatter = [NSDateFormatter new];
+  self.dateFormatter.dateFormat = BoSWeekdayDatePickerExampleDateFormatString;
+}
+
+- (void)displayInitialDateOnTheLabel
+{
+  NSAssert(self.dateFormatter, @"Label requires initialized date formatter.");
+  self.selectedDateLabel.text = [self.dateFormatter stringFromDate:self.exampleInitialDate];
+}
+
+- (void)setupWeekdayDatePicker
+{
+  BoSWeekdayDatePickerView *sourceCodePickerView = [[BoSWeekdayDatePickerView alloc] initWithFrame:self.datePickerFrame];
+
+  [sourceCodePickerView setupMinDate:self.exampleMinDate maxDate:self.exampleMaxDate initialSelectionDate:self.exampleInitialDate];
+  [self setupRowSelectionCallbackForDatePicker:sourceCodePickerView];
+
   [self.view addSubview:sourceCodePickerView];
 }
+
+- (void)setupRowSelectionCallbackForDatePicker:(BoSWeekdayDatePickerView *)weekdayDatePicker
+{
+  NSParameterAssert(weekdayDatePicker);
+
+  __weak typeof(self) weakSelf = self;
+  weekdayDatePicker.didSelectRowCallback = ^(BoSWeekdayDatePickerSelectedItems *selectedItems) {
+    weakSelf.selectedDateLabel.text = [weakSelf.dateFormatter stringFromDate:selectedItems.selectedDate];
+  };
+}
+
+- (NSDate *)exampleInitialDate
+{
+  return [NSDate bos_dateInCalendar:[BoSWeekdayDatePickerCalendar sharedInstance].calendar
+                            fromDay:15 month:12 year:2015];
+}
+
+- (NSDate *)exampleMinDate
+{
+  return [NSDate bos_dateInCalendar:[BoSWeekdayDatePickerCalendar sharedInstance].calendar
+                            fromDay:1 month:1 year:2005];
+}
+
+- (NSDate *)exampleMaxDate
+{
+  return [NSDate bos_dateInCalendar:[BoSWeekdayDatePickerCalendar sharedInstance].calendar
+                            fromDay:31 month:12 year:2016];
+}
+
+- (CGRect)datePickerFrame
+{
+  return CGRectMake(0.0, 336.0f, CGRectGetWidth([UIScreen mainScreen].bounds), 216.0f);
+}
+
 
 - (void)viewWillAppear:(BOOL)animated
 {
